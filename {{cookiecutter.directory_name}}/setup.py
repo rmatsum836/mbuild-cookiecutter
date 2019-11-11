90575c79161bd2cbe0c82eb430afdf01ff417ac3
Ray A. Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2019-11-11 17:02:25 -0500

    Update to setup.py

--- {{cookiecutter.directory_name}}/setup.py
+++ {{cookiecutter.directory_name}}/setup.py
@@ -1,11 +1,5 @@
-"""
-{{cookiecutter.project_name}}
-{{cookiecutter.description}}
-"""
-
 from setuptools import setup
 
-short_description = __doc__.split("\n")
 
 setup(
     # Self-descriptive entries which should always be present
@@ -14,11 +8,11 @@ setup(
     author_email='{{cookiecutter.author_email}}',
     license='{{cookiecutter.open_source_license}}',
     version='0.0.0',
-    description=short_description[0],
+    description='{{cookiecutter.description}}',
     zip_safe=False,
     entry_points={
         'mbuild.plugins':[
-        "FILL IN LATER"
+        "{{cookiecutter.first_plugin_name}} = {{cookiecutter.directory_name}}.{{cookiecutter.first_module_name}}:{{cookiecutter.first_plugin_name}}"
         ]
         }
     )
