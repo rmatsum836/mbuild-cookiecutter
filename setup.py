bd0ff801f55d5ec85551c7c1fd9861caf8499b1a
Ray A. Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2019-11-18 12:59:22 -0600

    Add files to test with tox

---
+++ setup.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+setup(
+        name="mbuild-cookiecutter",
+        version="0.0.0",
+        description="A Cookiecutter template for creating mBuild recipes",
+        author="Ray Matsumoto",
+        author_email="ray.a.matsumoto@vanderbilt.edu"
+    )
