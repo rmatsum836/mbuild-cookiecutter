8b6e3b76c5ba6f0b27aa76142ebc93923ca7e6b8
Ray A. Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2019-12-11 10:29:53 -0600

    Enforce naming convention in setup.py

---
+++ mbuild_{{ cookiecutter.project_name.lower().replace(' ', '_') }}/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(
+    # Self-descriptive entries which should always be present
+    name='mbuild_{{ cookiecutter.project_name.lower().replace(' ', '_') }}',
+    author='{{cookiecutter.author_name}}',
+    author_email='{{cookiecutter.author_email}}',
+    license='{{cookiecutter.open_source_license}}',
+    version='0.0.0',
+    description='{{cookiecutter.description}}',
+    zip_safe=False,
+    entry_points={
+        'mbuild.plugins':[
+        "{{cookiecutter.first_plugin_name}} = {{cookiecutter.directory_name}}.{{cookiecutter.first_module_name}}:{{cookiecutter.first_plugin_name}}"
+        ]
+        }
+    )
